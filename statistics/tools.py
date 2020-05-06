5bd2acbf20989b5501837736b8288b9b88fee212
Michal Hubert <michal.hubert77@gmail.com>
2020-05-06 19:20:03 +0200

    style checker

--- statistics/tools.py
+++ statistics/tools.py
@@ -1,21 +1,21 @@
 import scipy.stats as stats
 
 
-# Sort distributions according to how close they are to input data.
+# Sort distributions according to how close they are to input_data.
 # The closest one to data is the first.
-def sort_distributions(data):
+def sort_distributions(input_data):
     # See https://docs.scipy.org/doc/scipy/reference/stats.html
     distributions = ["exponweib", "norm", "pareto", "uniform", "logistic"]
     distribution_scoring = []
     distribution_parameters = {}
     for distribution_name in distributions:
-        dist = getattr(stats, distribution_name)
-        param = dist.fit(data)
+        distribution = getattr(stats, distribution_name)
+        param = distribution.fit(input_data)
         distribution_parameters[distribution_name] = param
-        _, p = stats.kstest(data, distribution_name, args=param)
+        _, p = stats.kstest(input_data, distribution_name, args=param)
         distribution_scoring.append((distribution_name, p))
 
-    distribution_scoring.sort(key=lambda elem: elem[1], reverse=True)
+    distribution_scoring.sort(key=lambda element: element[1], reverse=True)
     for elem in distribution_scoring:
         print("distribution:", elem[0], " p-value:", elem[1], "parameters:", distribution_parameters[elem[0]])
 
